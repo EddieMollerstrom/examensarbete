2f4d0286b0cd113ea4031c554c0ec9640d8e13ce
EddieMollerstrom <eddiemollerstrom123@gmail.com>
2025-05-01 16:06:12 +0200

    initial commit

---
+++ backend/examensarbete/src/main/kotlin/org/example/examensarbete/ExamensarbeteApplication.kt
@@ -0,0 +1,11 @@
+package org.example.examensarbete
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExamensarbeteApplication
+
+fun main(args: Array<String>) {
+    runApplication<ExamensarbeteApplication>(*args)
+}
